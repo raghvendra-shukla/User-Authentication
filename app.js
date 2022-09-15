4cab0597828b63f9041e980c0390188c7b658b29
Raghvendra Shukla <109507118+raghvendra-shukla@users.noreply.github.com>
2022-09-15 18:11:02 +0530

    update app.js

--- app.js
+++ app.js
@@ -12,8 +12,7 @@ require("./config/passport")(passport);
 
 //config
 // const db=require("./config-folder/keys").MongoURI;
-const password = encodeURIComponent("Raghvendra@123");
-const db=`mongodb+srv://Raghvendra-Shukla:${password}@raghvendra-cluster.nub3x7g.mongodb.net/?retryWrites=true&w=majority`;
+
 //mongo connection
 mongoose.connect(db,{useNewUrlParser:true})
     .then(()=>console.log("MongoDB is connected"))
